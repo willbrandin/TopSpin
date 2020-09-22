7fce0242f7acab9479230158f927950333424f23
willbrandin <will@tidelaundry.com>
2020-09-21 19:27:38 -0600

    Initial Commit

---
+++ TopSpin/TopSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopSpinApp.swift
+//  TopSpin
+//
+//  Created by Will Brandin on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct TopSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
